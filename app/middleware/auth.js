6e03b69b1c65a9d68d54ceb896abc39e54ba50db
Julien <julien.esnau@gmail.com>
2023-04-20 15:56:27 +0200

    Added env variable

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,23 +1,20 @@
-
-const jwt = require('jsonwebtoken')
-
- const verifyToken = async (req, res, next)=> {
-    
-    try {
-        let token = req.header("Authorization");
-        if(!token){
-            return res.status(403).send("Accès Interdit");
-        }
-        if(token.startsWith("Bearer")){
-            token = token.slice(7, token.length).trimLeft();
-        }
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
-        next();
-        
-    } catch(err){
-        console.log(err)
-        res.status(500).json({error: err.message})
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header('Authorization');
+    if (!token) {
+      return res.status(403).send('Accès Interdit');
     }
-}
+    if (token.startsWith('Bearer')) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
 module.exports = verifyToken;
