617e64eee9cee6c242d043893cad83bc9b6a4ee7
DUPIN <maximedupin1992@gmail.com>
2024-02-05 14:33:22 +0100

    fix

--- app/services/dbClient.js
+++ app/services/dbClient.js
@@ -1,8 +1,3 @@
-
-
-//  Comme pour Client les informations de connexion
-//   sont lu soit directement à partir de l'env soit donnée en paramêtre
-
 require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool();
